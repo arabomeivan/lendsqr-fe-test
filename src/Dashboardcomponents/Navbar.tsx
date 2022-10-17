00b0069f6af10c7744f0052e6e589048c8af3331
arabomeivan <arabomeivan@protonmail.com>
2022-10-17 11:29:51 +0100

    added elements for navbar

--- src/Dashboardcomponents/Navbar.tsx
+++ src/Dashboardcomponents/Navbar.tsx
@@ -6,17 +6,60 @@ export function Navbar()
 
         <nav className="navbar navbar-expand-lg navbar-light bg-white">
   <div className="container-fluid" id="navcontent">
+            
+{/*Logo Section*/}
+<div className="row" id='logo'>
+    <div className="col-md-12">
+  <img src="/public/assets/images/Union.png" alt="" />
+  <img src="/public/assets/images/lendsqr.png" alt="" />
+    </div>
+  </div>
 
+  
+  <div className="input-group mb-3">
+  <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"></input>
+  <span className="input-group-text" id="basic-addon1"><i className="fa-solid fa-magnifying-glass"></i></span>
+</div>
 
-
-    fnffkfkj
+    
     
 
       <form className="d-flex rightcontent">
-       
-         
 
-fjkffkn
+        {/**Docs*/}
+       <div className="row">
+        <div className="col-md-12">
+           <a href="#">Docs</a>
+        </div>
+       </div>
+        
+        {/**Notification Bell */}
+       <div className="row">
+        <div className="col-md-12">
+        <i className="fa-regular fa-bell"></i>
+        </div>
+       </div>
+
+       {/**Profile Picture*/}
+       <div className="row">
+        <div className="col-md-12">
+         
+        </div>
+       </div>
+
+       {/**Company Name*/}
+       <div className="row">
+        <div className="col-md-12">
+          Company Name
+        </div>
+       </div>
+
+       {/**DropDown */}
+       <div className="row">
+        <div className="col-md-12">
+        <i className="fa-solid fa-caret-down"></i>
+        </div>
+       </div>
       </form>
     </div>
 </nav>
