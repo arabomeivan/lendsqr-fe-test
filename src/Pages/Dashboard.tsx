7c10369db7f61ae23dfd28d80a11a2268b7f56be
arabomeivan <arabomeivan@protonmail.com>
2022-10-18 14:11:38 +0100

    formatted code for all components

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -1,29 +1,22 @@
-import '../Css/Dashboardcss/Dashboard.css';
-import {Navbar} from '../Dashboardcomponents/Navbar';
-import {Route, Routes, Link} from 'react-router-dom'
+import "../Css/Dashboardcss/Dashboard.css";
+import { Navbar } from "../Dashboardcomponents/Navbar";
+import { Route, Routes, Link } from "react-router-dom";
 
+import { Sidebarcontent } from "../Dashboardcomponents/Sidebarcontent";
 
-import {Sidebarcontent}from '../Dashboardcomponents/Sidebarcontent';
+export function Dashboard() {
+  return (
+    <>
+      <Navbar />
+      <div className="row">
+        <div className="col-sm-6" id="sidebar">
+          <Sidebarcontent />
+        </div>
 
-export function Dashboard()
-{
-    return(
-        
-
-            
-        <><Navbar/>
-        <div className="row">
-                <div className="col-sm-6" id="sidebar">
-                    
-                <Sidebarcontent/>
-                    
-                </div>
-
-                <div className="col-sm-6" id="maincontent">
-                    <div className="container">
-                        
-                    </div>
-                </div>
-            </div></>
-    );
+        <div className="col-sm-6" id="maincontent">
+          <div className="container"></div>
+        </div>
+      </div>
+    </>
+  );
 }
