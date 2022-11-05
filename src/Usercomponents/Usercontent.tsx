cfc20bf2a782ecf4740229e11f53055c6d39c180
arabomeivan <arabomeivan@protonmail.com>
2022-11-05 21:13:43 +0100

    did pagination and accordion for dropdown on sidebar

--- src/Usercomponents/Usercontent.tsx
+++ src/Usercomponents/Usercontent.tsx
@@ -1,83 +1,114 @@
-import '../Css/Usercontentcss/usercontent.css'
-import { Userstable } from './Userstable';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import "../Css/Usercontentcss/usercontent.css";
+import { Userstable } from "./Userstable";
 import coins from "../images/coins.png";
 import file from "../images/file.png";
 import users from "../images/users.png";
+import previous from "../images/forward.png"
+import next from "../images/previous.png"
 import activeusers from "../images/activeusers.png";
-export function Usercontent(props: any){
-    return(
+export function Usercontent(props: any) {
+  const loading = props.loading;
+  const tablevisibility = props.tablevisibility;
+
+  const totalusers = props.totalusers;
+  let usersperpage = props.usersperpage;
+  const pagenumbers = [];
+
+  let paginate = props.paginate
+
+  const pagenumberss = totalusers / usersperpage;
+
+  for (let i = 1; i <= pagenumberss; i++) {
+    pagenumbers.push(i);
+  }
+
+  return (
+    <div className="row">
+      <div className="col-sm-3" id="users">
+        <img src={users} alt="notloading" className="img-fluid icons" />
+
+        <div className="row" id="userstitle">
+          <div className="col-sm-12">USERS</div>
+        </div>
+
+        <div className="row numberofusers">
+          <div className="col-sm-12">{users.length}</div>
+        </div>
+      </div>
+
+      <div className="col-sm-3" id="activeusers">
+        <img src={activeusers} alt="notloading" className="img-fluid icons" />
+
+        <div className="row" id="activeuserstitle">
+          <div className="col-sm-12">ACTIVE USERS</div>
+        </div>
 
         <div className="row">
-            <div className="col-sm-3" id='users'>
-
-                 <img src={users} alt="notloading" className='img-fluid icons'/>
-                
-
-              <div className="row" id='userstitle'>
-                <div className="col-sm-12">
-                   USERS
-                </div>
-              </div>
-
-              <div className="row">
-                <div className="col-sm-12">
-
-                </div>
-              </div>
-
-            </div>
-
-            <div className="col-sm-3" id='activeusers'>
-
-            <img src={activeusers} alt="notloading" className='img-fluid icons'/>
-
-              <div className="row" id='activeuserstitle'>
-                <div className="col-sm-12">
-                   ACTIVE USERS
-                </div>
-              </div>
-
-              <div className="row">
-                <div className="col-sm-12">
-              
-                </div>
-              </div>
-            </div>
-            
-            <div className="col-sm-3" id='usersloans'>
-
-            <img src={file} alt="notloading" className='img-fluid icons'/>
-
-              <div className="row" id='usersloanstitle'>
-                <div className="col-sm-12">
-                     USERS WITH LOANS
-                </div>
-              </div>
-
-              <div className="row">
-                <div className="col-sm-12">
-
-                </div>
-              </div>
-            </div>
-            <div className="col-sm-3" id='usersavings'>
-                
-            <img src={coins} alt="notloading" className='img-fluid icons'/>
-
-              <div className="row" id='usersavingstitle'>
-                <div className="col-sm-12">
-                  USERS WITH SAVINGS
-                </div>
-              </div>
-
-              <div className="row">
-                <div className="col-sm-12">
-
-                </div>
-              </div>
-            </div>
-
-            <Userstable fetchedUsers={props.fetchedUsers}></Userstable>
+          <div className="col-sm-12"></div>
         </div>
-    );
+      </div>
+
+      <div className="col-sm-3" id="usersloans">
+        <img src={file} alt="notloading" className="img-fluid icons" />
+
+        <div className="row" id="usersloanstitle">
+          <div className="col-sm-12">USERS WITH LOANS</div>
+        </div>
+
+        <div className="row">
+          <div className="col-sm-12"></div>
+        </div>
+      </div>
+      <div className="col-sm-3" id="usersavings">
+        <img src={coins} alt="notloading" className="img-fluid icons" />
+
+        <div className="row" id="usersavingstitle">
+          <div className="col-sm-12">USERS WITH SAVINGS</div>
+        </div>
+
+        <div className="row">
+          <div className="col-sm-12"></div>
+        </div>
+      </div>
+
+      <Userstable
+        fetchedUsers={props.fetchedUsers}
+        loading={loading}
+        tablevisibility={tablevisibility}
+      ></Userstable>
+
+      {/** Pagination List*/}
+
+      
+        <nav aria-label="Page navigation example">
+
+          
+          <ul className="pagination">
+            <li className="page-item">
+              <a className="page-link" href="#" id="previous">
+                <img className="img-fluid historymode" src={next} alt="" />
+              </a>
+            </li>
+           
+            {
+      pagenumbers.map((number, index:number) => (
+            <li className="page-item" key={index}>
+              <a className="page-link" href="#" onClick={()=> paginate(number)} id='numberitems'>
+                {number}
+              </a>
+            </li>
+
+     )) }
+
+            <li className="page-item">
+              <a className="page-link" href="#" id="next">
+              <img className="img-fluid historymode" src={previous} alt="" />
+              </a>
+            </li>
+          </ul>
+        </nav>
+      
+    </div>
+  );
 }
--- src/Views/Users.tsx
+++ src/Views/Users.tsx
@@ -5,23 +5,50 @@ import { Usercontent } from "../Usercomponents/Usercontent";
 import { Navbar } from "../Dashboardcomponents/Navbar";
 import { Sidebarcontent } from "../Dashboardcomponents/Sidebarcontent";
 import axios from 'axios'
-import { useState, useEffect } from "react";
+import { useState, useEffect, SetStateAction } from "react";
 
 export function Users() {
 
 
   const url = 'https://6270020422c706a0ae70b72c.mockapi.io/lendsqr/api/v1/users'
-  const [users, setusers] = useState([])
+  const [users, setUsers] = useState([])
+
+  const [loading, setloading] = useState(true)
+  const [tablevisibility, settablevisibility] = useState(false)
+
+  //Pagination to display 10 users per page
+
+  const [pagenumber, setpagenumber] = useState(1)
+  const [usersperpage, setusersperpage] = useState(10)
+ 
+
+
+
+
 
   useEffect(()=>
   {
-    axios.get(url).then(response =>
-      {
-        setusers(response.data)
+    axios.get(url).then(Response=>
+      { 
+         setUsers(Response.data)
+         
+         setloading(false)
+         settablevisibility(true)
       })
-      
-  }, [url])
 
+
+
+  }, [])
+  
+
+  const indexoflastpage = pagenumber * usersperpage
+  const indexoffirstpage = indexoflastpage - usersperpage
+  let [currentusers] = useState([])
+
+  currentusers = users.slice(indexoffirstpage, indexoflastpage)
+ 
+ 
+  const paginate = (pageNumber:number)=> setpagenumber(pageNumber)
   
 
   return (
@@ -36,7 +63,7 @@ export function Users() {
 
         <div className="col-sm-6" id="maincontent">
           <div className="container" id="usercontent">
-            <Usercontent fetchedUsers={users}></Usercontent>
+            <Usercontent fetchedUsers={currentusers} loading={loading} tablevisibility={tablevisibility}  usersperpage={usersperpage} totalusers={users.length} paginate={paginate}></Usercontent>
           </div>
         </div>
       </div>
